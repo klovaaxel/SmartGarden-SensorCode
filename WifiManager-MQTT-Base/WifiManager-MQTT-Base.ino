8330774e341d43899006b0f4a83b6e27d45c403c
klovaaxel <klovakarlsson@gmail.com>
2020-02-18 10:06:42 +0100

    Cleaner and clearer code
    removed unnecesary lines and changed others to work with new html and css

--- WifiManager-MQTT-Base/WifiManager-MQTT-Base.ino
+++ WifiManager-MQTT-Base/WifiManager-MQTT-Base.ino
@@ -1,29 +1,28 @@
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <ESP8266WiFi.h>    //https://github.com/esp8266/Arduino
+//for storing data
+#include "EEPROM.h"
 
 //needed for library
-#include <DNSServer.h>
-#if defined(ESP8266)
-#include <ESP8266WebServer.h>
-#else
-#include <WebServer.h>
-#endif
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+  #include <DNSServer.h>
+  #include <ESP8266WebServer.h>
+  #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
 //MQTT 
-#include <PubSubClient.h>
+  #include <PubSubClient.h>
+  std::string sensNum = "/klovakarlsson@gmail.com/01001"; //----------------------------------------------Change This
 
 //mosit
-int moistPin = A0;
-String moist_str;
-char moist[50];
+  int moistPin = A0;
+  String moist_str;
+  char moist[50];
 
 //temp
-#include <dht.h>
-dht DHT;
-#define DHT11_PIN 2
-
-String temp_str;
-char temp[50];
+  #include <dht.h>
+  dht DHT;
+  #define DHT11_PIN 2
+  
+  String temp_str;
+  char temp[50];
 
 const char* mqtt_server = "mqtt.dioty.co";
 
@@ -37,10 +36,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
 
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
@@ -64,9 +59,8 @@ void reconnect() {
     if (client.connect(clientId.c_str(),"klovakarlsson@gmail.com", "ee07432c")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("/klovakarlsson@gmail.com/hej", "hello world");
       // ... and resubscribe
-      client.subscribe("/klovakarlsson@gmail.com/hej");
+      client.subscribe("/klovakarlsson@gmail.com/01001");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -83,6 +77,10 @@ void setup() {
     //pinMode(inPin, INPUT);
     // put your setup code here, to run once:
     Serial.begin(115200);
+    Serial.println("serial started");
+
+    EEPROM.begin(512);
+
 
     //WiFiManager
     //Local intialization. Once its business is done, there is no need to keep it around
@@ -97,50 +95,56 @@ void setup() {
     //if it does not connect it starts an access point with the specified name
     //here  "AutoConnectAP"
     //and goes into a blocking loop awaiting configuration
-    wifiManager.autoConnect("AutoConnectAP");
-    //or use this for auto generated name ESP + ChipID
-    //wifiManager.autoConnect();
-
+    wifiManager.autoConnect("SmartSensor1.0AP");
     
     //if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
+    Serial.println("connected");
 
 
     client.setServer(mqtt_server, 1883);
     client.setCallback(callback);
-    Serial.println("MQTT...yeey :)");
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  int sensorValue = analogRead(moistPin);
-//  String stringOne = "Sensor value: ";
-//  String moistMessage = stringOne + sensorValue;
+  
   //moist
+  int sensorValue = analogRead(moistPin);
+  sensorValue = 100 - (sensorValue / 10);
   Serial.println(sensorValue);
-  moist_str = String(sensorValue); //converting ftemp (the float variable above) to a string
+  moist_str = String(sensorValue) + "%"; //converting ftemp (the float variable above) to a string
+  Serial.println(moist_str);
   moist_str.toCharArray(moist, moist_str.length() + 1);
   
   //temp
   int chk = DHT.read11(DHT11_PIN);
-  temp_str = String(DHT.temperature); //converting ftemp (the float variable above) to a string
+  temp_str = String(DHT.temperature) + "°C"; //converting ftemp (the float variable above) to a string
   temp_str.toCharArray(temp, temp_str.length() + 1);
   
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
   long now = millis();
+  
   if (now - lastMsg > 2000) {
     lastMsg = now;
     ++value;
-    //snprintf (msg, 50, "hello", value);
     Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("/klovakarlsson@gmail.com/temp", temp);
-    client.publish("/klovakarlsson@gmail.com/moist", moist);
+    Serial.print(temp);
+    Serial.print(", ");
+    Serial.print(moist);
+    Serial.println();
+    
+    std::string moistChannel = sensNum + "/moist";
+    std::string tempChannel = sensNum + "/temp";
+
+    Serial.println(moistChannel.c_str());
+
+
+    client.publish((tempChannel.c_str()), temp);
+    client.publish((moistChannel.c_str()), moist);
+    delay(5000);
   }
+
 }
