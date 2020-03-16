810f12e9021f5ef9f9b8930edd968bfe3087c38c
klovaaxel <klovakarlsson@gmail.com>
2020-03-16 13:53:09 +0100

    latest code with commenting

--- WifiManager-MQTT-Base/WifiManager-MQTT-Base.ino
+++ WifiManager-MQTT-Base/WifiManager-MQTT-Base.ino
@@ -1,38 +1,42 @@
 #include <ESP8266WiFi.h>    //https://github.com/esp8266/Arduino
-//for storing data
-#include "EEPROM.h"
 
-//needed for library
+#include "EEPROM.h" //for storing data in non volitile memory
+
+//WIFI - needed for wifi server and connection
   #include <DNSServer.h>
   #include <ESP8266WebServer.h>
-  #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+  #include <WiFiManager.h>  //https://github.com/tzapu/WiFiManager
 
-//MQTT 
+//MQTT - (communication)
   #include <PubSubClient.h>
-  std::string sensNum = "/klovakarlsson@gmail.com/01001"; //----------------------------------------------Change This
+  std::string sensNum = "/klovakarlsson@gmail.com/01001"; // Sens MQTT channel and sens number //------------Change This 
+  const char* sensNumChar = sensNum.c_str(); //makes sensNum a const char
+  
+  const char* email = "klovakarlsson@gmail.com"; //mqtt email
+  const char* password = "ee07432c"; //mqtt password (recived via mail)
 
 //mosit
-  int moistPin = A0;
-  String moist_str;
-  char moist[50];
+  int moistPin = A0;  // Pin that moist senor is connected to (A0 = ANALOG 0)
+  String moist_str; // String that stores MositSens value
+  char moist[50]; // Same as Moist_string but in format compatible with MQTT
 
 //temp
-  #include <dht.h>
-  dht DHT;
-  #define DHT11_PIN 2
-  
-  String temp_str;
-  char temp[50];
+  #include <dht.h>  // Tempsensor libary
+  dht DHT;  //Defines DHT
+  #define DHT11_PIN 2 // Pin that Temp Sensor is connected to
+   
+  String temp_str; // String that stores Temp value
+  char temp[50]; // Same as temp_str but in format compatible with MQTT
 
-const char* mqtt_server = "mqtt.dioty.co";
+const char* mqtt_server = "mqtt.dioty.co";  // the MQTT Broker address
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
+WiFiClient espClient; // Defines Wifi to be ESP 
+PubSubClient client(espClient); //Defines pubsubclient to use Wifi from espClient
 
-void callback(char* topic, byte* payload, unsigned int length) {
+long lastMsg = 0; // MQTT
+char msg[50]; // MQTT
+int value = 0;  // MQTT
+void callback(char* topic, byte* payload, unsigned int length) { //function called when recieves message from subbed mqtt connection --- NOT USED 
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -40,51 +44,48 @@ void callback(char* topic, byte* payload, unsigned int length) {
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
     digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
   } else {
     digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
 
 }
 
-void reconnect() {
+void reconnect() {  //Reconnect to MQTT broker
   // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
+  while (!client.connected()) { //while not conneted
+    Serial.print("Attempting MQTT connection..."); //prints to serial 
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str(),"klovakarlsson@gmail.com", "ee07432c")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      // ... and resubscribe
-      client.subscribe("/klovakarlsson@gmail.com/01001");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
+    
+    if (client.connect(clientId.c_str(), email, password)) { //Attempts to connect to MQTT broker with email and password 
+
+      Serial.println("connected");  // Once connected, publish an announcement...
+      client.subscribe(sensNumChar);  // ... and resubscribe to sens channel and num
+
+    } else { // if connection fails 
+     
+      Serial.print("failed, rc=");  
+      Serial.print(client.state()); //prints connection error to serial
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+    
+      delay(5000);  // Wait 5 seconds before retrying
     }
   }
 }
 
-void setup() {
+void setup() {  //code runs once at setup
 
-    //moist
-    //pinMode(inPin, INPUT);
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    Serial.println("serial started");
+    Serial.begin(115200); //Starts serial at 115200
+    Serial.println("serial started"); //prints to serial
 
-    EEPROM.begin(512);
+    EEPROM.begin(512);  //starts EEPROM at 512 (makes non volitile memmory available)
 
 
     //WiFiManager
     //Local intialization. Once its business is done, there is no need to keep it around
     WiFiManager wifiManager;
+    
     //reset saved settings
     //wifiManager.resetSettings();
     
@@ -93,41 +94,41 @@ void setup() {
 
     //fetches ssid and pass from eeprom and tries to connect
     //if it does not connect it starts an access point with the specified name
-    //here  "AutoConnectAP"
+    //here  "SmartSensor1.0AP"
     //and goes into a blocking loop awaiting configuration
-    wifiManager.autoConnect("SmartSensor1.0AP");
+    wifiManager.autoConnect("SmartSensor1.0AP");  
     
     //if you get here you have connected to the WiFi
     Serial.println("connected");
 
-
+    //sets mqtt server to correct websocket and callback
     client.setServer(mqtt_server, 1883);
     client.setCallback(callback);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { // put your main code here, to run repeatedly:
   
   //moist
-  int sensorValue = analogRead(moistPin);
-  sensorValue = 100 - (sensorValue / 10);
-  Serial.println(sensorValue);
+  int sensorValue = analogRead(moistPin); //reads moist sensor
+  sensorValue = 100 - (sensorValue / 10); //converts senorValue to a precentage
+  //Serial.println(sensorValue);  //debug the moist value 
   moist_str = String(sensorValue) + "%"; //converting ftemp (the float variable above) to a string
-  Serial.println(moist_str);
-  moist_str.toCharArray(moist, moist_str.length() + 1);
+  //Serial.println(moist_str);  //debug the moist value 
+  moist_str.toCharArray(moist, moist_str.length() + 1); //converts string to char to be compatible with mqtt library
   
   //temp
-  int chk = DHT.read11(DHT11_PIN);
+  int chk = DHT.read11(DHT11_PIN);  //reads temp sensor
   temp_str = String(DHT.temperature) + "°C"; //converting ftemp (the float variable above) to a string
-  temp_str.toCharArray(temp, temp_str.length() + 1);
+  temp_str.toCharArray(temp, temp_str.length() + 1);  //converts string to char to be compatible with mqtt library
   
-  if (!client.connected()) {
+  if (!client.connected()) {  //if client not connected call reconnect function
     reconnect();
   }
+  
   client.loop();
   long now = millis();
   
-  if (now - lastMsg > 2000) {
+  if (now - lastMsg > 2000) { //to not spam the mqtt (is bad)
     lastMsg = now;
     ++value;
     Serial.print("Publish message: ");
@@ -136,15 +137,16 @@ void loop() {
     Serial.print(moist);
     Serial.println();
     
-    std::string moistChannel = sensNum + "/moist";
-    std::string tempChannel = sensNum + "/temp";
+    std::string moistChannel = sensNum + "/moist"; //creates a channel for moist value
+    std::string tempChannel = sensNum + "/temp";  //creates a channel for temp value
 
-    Serial.println(moistChannel.c_str());
+    //Serial.println(moistChannel.c_str()); //debug for moist channel
 
 
-    client.publish((tempChannel.c_str()), temp);
-    client.publish((moistChannel.c_str()), moist);
-    delay(5000);
+    client.publish((tempChannel.c_str()), temp);  // publishes temp to temp channel on mqtt
+    client.publish((moistChannel.c_str()), moist);  // publishes moist to moist channel on mqtt
+    
+    delay(5000); //waits for 5seconds (should idealy sleep for a set amount of time to save battery)
   }
 
 }
